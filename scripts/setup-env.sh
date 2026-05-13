0d5af88beefacf6c8e9b6ca3fbb36e3faf4507e4
Riccardo Valtorta <riccardo.valtorta@vhit-weifu.com>
2026-05-13 10:01:45 +0200

    Automated consumer snap environment sourcing

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -18,6 +18,15 @@ source $ROS_BASE/opt/ros/humble/setup.bash
 # Overlay SECOND
 source $PROVIDER_SNAP/local_setup.bash
 
+# Consumer Python
+if [ -d "$SNAP/opt/ros/humble" ]; then
+    export PYTHONPATH=$SNAP/opt/ros/humble/lib/python3.10/site-packages:$PYTHONPATH
+    export PYTHONPATH=$SNAP/opt/ros/humble/local/lib/python3.10/dist-packages:$PYTHONPATH
+
+    export LD_LIBRARY_PATH=$SNAP/opt/ros/humble/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$SNAP/opt/ros/humble/lib/$TRIPLET:$LD_LIBRARY_PATH
+fi
+
 # Python
 export PYTHONPATH=$ROS_BASE/lib/python3.10/site-packages:$PYTHONPATH
 export PYTHONPATH=$PROVIDER_SNAP/opt/ros/humble/lib/python3.10/site-packages:$PYTHONPATH
@@ -32,6 +41,11 @@ export LD_LIBRARY_PATH=$ROS_BASE/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROS_BASE/lib/$TRIPLET:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROS_BASE/usr/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROS_BASE/usr/lib/$TRIPLET:$LD_LIBRARY_PATH
+
+# Consumer overlay FIRST (if present)
+if [ -d "$SNAP/opt/ros/humble" ]; then
+    export AMENT_PREFIX_PATH=$AMENT_PREFIX_PATH:$SNAP/opt/ros/humble
+fi
 
 # Overlay
 export AMENT_PREFIX_PATH=$AMENT_PREFIX_PATH:$PROVIDER_SNAP/opt/ros/humble
