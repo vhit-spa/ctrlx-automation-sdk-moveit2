2bfdec1837477fa6ff482ff95ca594ad3207dc62
Riccardo Valtorta <riccardo.valtorta@vhit-weifu.com>
2026-05-28 15:55:38 +0200

    Headless implementation

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -22,6 +22,7 @@ source $PROVIDER_SNAP/local_setup.bash
 if [ -d "$SNAP/opt/ros/humble" ]; then
     export PYTHONPATH=$SNAP/opt/ros/humble/lib/python3.10/site-packages:$PYTHONPATH
     export PYTHONPATH=$SNAP/opt/ros/humble/local/lib/python3.10/dist-packages:$PYTHONPATH
+    export PYTHONPATH=$SNAP/usr/lib/python3/dist-packages:$PYTHONPATH
 
     export LD_LIBRARY_PATH=$SNAP/opt/ros/humble/lib:$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=$SNAP/opt/ros/humble/lib/$TRIPLET:$LD_LIBRARY_PATH
@@ -29,8 +30,11 @@ fi
 
 # Python
 export PYTHONPATH=$ROS_BASE/lib/python3.10/site-packages:$PYTHONPATH
+export PYTHONPATH=$ROS_BASE/usr/lib/python3/dist-packages:$PYTHONPATH
+
 export PYTHONPATH=$PROVIDER_SNAP/opt/ros/humble/lib/python3.10/site-packages:$PYTHONPATH
 export PYTHONPATH=$PROVIDER_SNAP/opt/ros/humble/local/lib/python3.10/dist-packages:$PYTHONPATH
+export PYTHONPATH=$PROVIDER_SNAP/usr/lib/python3/dist-packages:$PYTHONPATH
 
 # Provider ROS libs
 export LD_LIBRARY_PATH=$PROVIDER_SNAP/opt/ros/humble/lib:$LD_LIBRARY_PATH
@@ -43,12 +47,16 @@ export LD_LIBRARY_PATH=$PROVIDER_SNAP/opt/ros/humble/lib/controller_manager:$LD_
 # Provider system libs
 export LD_LIBRARY_PATH=$PROVIDER_SNAP/usr/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$PROVIDER_SNAP/usr/lib/$TRIPLET:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PROVIDER_SNAP/usr/lib/$TRIPLET/blas:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PROVIDER_SNAP/usr/lib/$TRIPLET/lapack:$LD_LIBRARY_PATH
 
 # Base provider libs
 export LD_LIBRARY_PATH=$ROS_BASE/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROS_BASE/lib/$TRIPLET:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROS_BASE/usr/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROS_BASE/usr/lib/$TRIPLET:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$ROS_BASE/usr/lib/$TRIPLET/blas:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$ROS_BASE/usr/lib/$TRIPLET/lapack:$LD_LIBRARY_PATH
 
 
 # Consumer overlay FIRST (if present)
